6cf26bc6ab88d704b4c2373a9c9bf4bbd9ca5c3a
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2023-02-19 17:02:23 -0600

    Postfix
    Implementacion de shunting yard para regex

---
+++ src/alfabeto.py
@@ -0,0 +1,17 @@
+'''
+*************************************************
+Universidad del Valle de Guatemala
+Diseño de Lenguajes de Programación
+
+alfabeto.py
+- Definicion de alfabeto para regex
+
+Autor: Diego Cordova - 20212
+*************************************************
+'''
+
+lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z']
+upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+digits = ['0','1','2','3','4','5','6','7','8','9']
+
+ALPHABET = lower + upper + digits
---
+++ src/postfix.py
@@ -0,0 +1,91 @@
+'''
+*************************************************
+Universidad del Valle de Guatemala
+Diseño de Lenguajes de Programación
+
+postfix.py
+- Implementación del algoritmo shunting yard
+  para conversion de infix a postfix para regex
+
+Autor: Diego Cordova - 20212
+*************************************************
+'''
+
+from .alfabeto import ALPHABET
+
+_ORDER = {
+  '*': 1,
+  '|': 2,
+  '.': 2,
+  '(': 0,
+}
+
+def _shunting(regex:str) -> list:
+  '''
+  Implementacion del algoritmo Shunting Yard adaptado a regex
+  Ademas, agrega operador . al postfix final
+  '''
+  regex = regex.replace(' ', '')
+  regex = list(regex)
+  out:list = [] # Stack de salida
+  stack:list = [] # Stack de stackeradores
+
+  while len(regex) > 0:
+    char = regex.pop(0)
+
+    if char in ALPHABET:
+      if len(out) > 0 and '|' not in stack:
+        last_char = out[-1]
+        if (
+          last_char in ALPHABET
+          or last_char in ['*', '?']
+        ):
+          stack.append('.')
+
+      out.append(char)
+  
+    elif char == '(':
+      stack.append(char)
+
+    elif char == ')':
+      next_char = stack.pop()
+
+      while next_char != '(':
+        out.append(next_char)
+        next_char = stack.pop()
+        
+        if next_char != '(' and len(stack) == 0: raise Exception('Error: regex not valid.\n"(" missing')
+      
+      if len(regex) > 0:
+        next_char = regex[0]
+        if next_char in ['*', '?']:
+          regex.pop(0)
+          if next_char != out[-1]:
+            out.append(next_char)
+
+    elif char in ['*', '?']:
+      if char != out[-1]:
+        out.append(char)
+
+    elif char in ['|', '.']:
+      if char == out[-1]: continue
+
+      last_op = stack[-1]
+      if _ORDER[char] <= _ORDER[last_op]:
+        stack.pop()
+        out.append(last_op)
+
+      stack.append(char)
+
+  while len(stack) > 0:
+    actual_stack = stack.pop()
+    if actual_stack == out[-1]: continue
+    out.append(actual_stack)
+
+    if actual_stack in ['(', ')']: raise Exception('Error: regex not valid.\n"(" missing')
+
+  return out
+
+def toPostfix(regex:str) -> list:
+  '''Devuelve la representacion en postfix de una regex en infix'''
+  return _shunting(regex)
