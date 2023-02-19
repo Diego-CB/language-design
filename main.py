c618e36315719bd908740abd6e65e408556f0c60
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2023-02-19 17:15:22 -0600

    Code Formatting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src import *
 
 if __name__ == '__main__':
-  prueba = 'ab*ab*'
-  r = toPostfix(prueba)
-  print(r)
+    prueba = 'ab*ab*'
+    r = toPostfix(prueba)
+    print(r)
--- src/postfix.py
+++ src/postfix.py
@@ -21,76 +21,76 @@ _ORDER = {
 }
 
 def _shunting(regex:str) -> list:
-  '''
-  Implementacion del algoritmo Shunting Yard adaptado a regex
-  Ademas, agrega operador . al postfix final
-  Referencia: https://www.cs.buap.mx/~andrex/estructuras/AlgoritmoPolacasPosfijo.pdf
-  '''
-  regex = regex.replace(' ', '')
-  regex = list(regex)
-  out:list = [] # Stack de salida
-  stack:list = [] # Stack de stack de operadores
+    '''
+    Implementacion del algoritmo Shunting Yard adaptado a regex
+    Ademas, agrega operador . al postfix final
+    Referencia: https://www.cs.buap.mx/~andrex/estructuras/AlgoritmoPolacasPosfijo.pdf
+    '''
+    regex = regex.replace(' ', '')
+    regex = list(regex)
+    out:list = [] # Stack de salida
+    stack:list = [] # Stack de stack de operadores
 
-  while len(regex) > 0:
-    char = regex.pop(0)
+    while len(regex) > 0:
+        char = regex.pop(0)
 
-    if char in ALPHABET:
-      if len(out) > 0 and '|' not in stack:
-        last_char = out[-1]
-        if (
-          last_char in ALPHABET
-          or last_char in ['*', '?']
-        ):
-          stack.append('.')
+        if char in ALPHABET:
+            if len(out) > 0 and '|' not in stack:
+                last_char = out[-1]
+                if (
+                    last_char in ALPHABET
+                    or last_char in ['*', '?']
+                ):
+                    stack.append('.')
 
-      out.append(char)
-  
-    elif char == '(':
-      stack.append(char)
+            out.append(char)
 
-    elif char == ')':
-      next_char = stack.pop()
+        elif char == '(':
+            stack.append(char)
 
-      while next_char != '(':
-        out.append(next_char)
-        next_char = stack.pop()
-        
-        if next_char != '(' and len(stack) == 0:
-          raise Exception('Error: regex not valid.\n"(" missing')
-      
-      if len(regex) > 0:
-        next_char = regex[0]
-        if next_char in ['*', '?']:
-          regex.pop(0)
-          if next_char != out[-1]:
-            out.append(next_char)
+        elif char == ')':
+            next_char = stack.pop()
 
-    elif char in ['*', '?']:
-      if char != out[-1]:
-        out.append(char)
+            while next_char != '(':
+                out.append(next_char)
+                next_char = stack.pop()
+                
+                if next_char != '(' and len(stack) == 0:
+                    raise Exception('Error: regex not valid.\n"(" missing')
+            
+            if len(regex) > 0:
+                next_char = regex[0]
+                if next_char in ['*', '?']:
+                    regex.pop(0)
+                    if next_char != out[-1]:
+                        out.append(next_char)
 
-    elif char in ['|', '.']:
-      if char == out[-1]: continue
+        elif char in ['*', '?']:
+            if char != out[-1]:
+                out.append(char)
 
-      last_op = stack[-1]
-      if _ORDER[char] <= _ORDER[last_op]:
-        stack.pop()
-        out.append(last_op)
+        elif char in ['|', '.']:
+            if char == out[-1]: continue
 
-      stack.append(char)
+            last_op = stack[-1]
+            if _ORDER[char] <= _ORDER[last_op]:
+                stack.pop()
+                out.append(last_op)
+
+            stack.append(char)
     
-    else:
-      raise Exception(f'Simbol not valid for regex: {char}')
+        else:
+            raise Exception(f'Simbol not valid for regex: {char}')
 
-  while len(stack) > 0:
-    actual_stack = stack.pop()
-    if actual_stack == out[-1]: continue
-    out.append(actual_stack)
+    while len(stack) > 0:
+        actual_stack = stack.pop()
+        if actual_stack == out[-1]: continue
+        out.append(actual_stack)
 
-    if actual_stack in ['(', ')']: raise Exception('Error: regex not valid.\n"(" missing')
+        if actual_stack in ['(', ')']: raise Exception('Error: regex not valid.\n"(" missing')
 
-  return out
+    return out
 
 def toPostfix(regex:str) -> list:
-  '''Devuelve la representacion en postfix de una regex en infix'''
-  return _shunting(regex)
+    '''Devuelve la representacion en postfix de una regex en infix'''
+    return _shunting(regex)
