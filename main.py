53a0ff4c75cca565280702fbcc771f023a294fd6
Diego-ACB <60157586+Nes15442@users.noreply.github.com>
2023-04-07 14:18:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ from src import *
 import sys
 
 if __name__ == '__main__':
-    filepath = './Examples/slr-2.yal'
-    reader: YalexReader = YalexReader(filename=filepath)
-    print(reader)
-
-else:
-    if len(sys.argv) < 2:
-        raise Exception('Input Yalex File Missing')
-
-    # Read Input File
-    filepath = sys.argv[1]
-    reader: YalexReader = YalexReader(filename=filepath)
-    print(reader)
+    filepath = './Examples/slr-1.yal'
+    filepath = './Examples/xd.yal'
+    ReadYalex(filepath=filepath)
+    # print(reader)
