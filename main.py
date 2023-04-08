b872c7ce6d75ddfeeaf0eaebd0a33e5537ace2a0
Diego-ACB <60157586+Nes15442@users.noreply.github.com>
2023-04-07 21:43:12 -0600

    main Completed

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from src import *
 import sys
 
 if __name__ == '__main__':
-    filepath = './Examples/xd.yal'
-    filepath = './Examples/slr-1.yal'
-    filepath = './Examples/slr-2.yal'
-    filepath = './Examples/slr-3.yal'
-    filepath = './Examples/xd2.yal'
-    filepath = './Examples/slr-4.yal'
+
+    if len(sys.argv) < 2:
+        raise Exception('Input Yalex File Missing')
+
+    filepath = sys.argv[1]
     ReadYalex(filepath=filepath)
+
+# filepath = './Examples/slr-4.yal'
+# filepath = './Examples/slr-1.yal'
+# filepath = './Examples/xd.yal'
+# filepath = './Examples/slr-2.yal'
+# filepath = './Examples/slr-3.yal'
+# filepath = './Examples/xd2.yal'
