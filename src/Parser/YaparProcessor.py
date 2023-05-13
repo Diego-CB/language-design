2307cad6d5c5b52b26065392766e7926770c2254
Diego-ACB <60157586+Nes15442@users.noreply.github.com>
2023-05-13 13:51:36 -0600

    avances en parser

--- src/Parser/YaparProcessor.py
+++ src/Parser/YaparProcessor.py
@@ -1,31 +1,4 @@
-
-class Prod:
-    def __init__(self) -> None:
-        self.name: None
-        self.productions: None
-
-    def __repr__(self) -> str:
-        return f'{self.name} -> {self.productions}'
-
-
-def processLines(tokenLines: list[str]) -> None:
-    tokens: list = []
-    productions: list = []
-
-    for line in tokenLines:
-        if line[0] == 'TOKEN':
-            new_tokens = _process_token(line[1])
-            for token in new_tokens:
-                tokens.append(token)
-
-        elif line[0] == 'PRODUCTION':
-            new_prod = _process_production(line[1])
-            productions.append(new_prod)
-
-    print(tokens)
-
-    for p in productions:
-        print(p)
+from copy import copy as cp
 
 
 def _process_token(line: str) -> list[str]:
@@ -36,17 +9,58 @@ def _process_token(line: str) -> list[str]:
 
 
 def _process_production(line: str) -> list[str]:
-    new_prod = Prod()
+    new_prod = {}
     line = line.replace('/n', '')
     line = line.replace('/', '')
     line = line.replace(';', '')
     prod_name, line = line.split(':')
-    new_prod.name = (prod_name)
     productions = line.split('|')
-    productions = [_proces_partProd(p) for p in productions]
-    new_prod.productions = (productions)
+    new_prod[prod_name] = [_proces_partProd(p) for p in productions]
     return new_prod
 
 
 def _proces_partProd(prod: str) -> list[str]:
     return [token for token in prod.split(' ') if token != '']
+
+
+def processLines(tokenLines: list[str]) -> None:
+    tokens: list = []
+    productions: dict = {}
+
+    for line in tokenLines:
+        if line[0] == 'TOKEN':
+            new_tokens = _process_token(line[1])
+            for token in new_tokens:
+                tokens.append(token)
+
+        elif line[0] == 'PRODUCTION':
+            new_prod = _process_production(line[1])
+            productions.update(new_prod)
+
+    print(tokens)
+    productions = _itemsfromProd(productions)
+
+    for p in productions.keys():
+        print(p, '->', productions[p])
+
+
+def _itemsfromProd(prod: dict) -> dict:
+    new_items: dict = {}
+    for k in prod.keys():
+        new_items[k] = _getitems(prod[k])
+
+    return new_items
+
+
+def _getitems(prods):
+    new_prods = []
+
+    # TODO reemplazar insert con funcion custom que no reemplaze la data
+    #      sino la mueva para poner el "."
+    for prod in prods:
+        for n in range(len(prod)):
+            og: list = cp(prod)
+            og.insert(n, '.')
+            new_prods.append(og)
+
+    return new_prods
